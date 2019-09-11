352628444189156b93352437843709c68ac87eca
Emmanuel Rocha <emmanuelrocha001@gmail.com>
2019-09-10 23:08:48 -0500

    refactored app logic. Improved grid creation, and scaling. Implemented view matrix. Performance optimization, display is only updated as needed. temporary removal of grid selection.

--- map-visualization/app.js
+++ map-visualization/app.js
@@ -1,146 +1,171 @@
-// Jimp.read('../assets/test-map.png')
-// .then(image => {
-//     // console.log('read image successfully');
-//     // console.log(image);
-//     console.log(Jimp.intToRGBA(image.getPixelColor(32,32))["r"]);
-//     // image.greyscale()
-//     //     .write('cropped-test.png');
-//     // // Do stuff with the image.
-// })
-// .catch(err => {
-//     console.log('broken');
-//     // Handle an exception.
-// });
-
 // create instance of p5
 new p5();
 var increment_size;
-const TRANSPERENCY = 50;
-// const Jimp = require('jimp');
+
+
+// canvas and grid info
+/*----------------------------------------------------------*/
+var CANVAS_HEIGHT = 800;
+var CANVAS_WIDTH = 800;
+// var CANVAS_HEIGHT = 2000;
+// var CANVAS_WIDTH = 2000;
+
+
+const GRID_PROCESSING_VALUE = 2;
+// contains grid of image processed at an initial chunk value
+var grid;
+
+var draw_grid = true;
+
+// colors
 const COLOR_BLACK = color(0, 0, 0);
-//map generation colors
+const COLOR_GRAY = color(242, 242, 242);
+const COLOR_WHITE = color(255, 255, 255);
+const COLOR_BLUE = color(135, 206, 235, 100);
+
+//selection square info
+const TRANSPERENCY = 50;
+const SELECTION_COLOR = color(0, 0, 100, TRANSPERENCY);
+
+
+// map generation variables
+/*----------------------------------------------------------*/
+// general colors
 const COLOR_ROAD = color(246, 246, 234);
 const COLOR_GRASS = color(194, 221, 168);
 const COLOR_BUILDING = color(255, 212,86);
 const COLOR_LETTER = color(107, 92, 113 );
-// const COLOR_LETTER = color()
-//li
+const COLOR_PURPLE = color(0, 91, 156);
+
+// pixel liniency
 const PIXEL_VALUE_LINIENCY = 5;
 const LETTER_PIXEL_VALUE_LINIENCY = 75;
 
-const COLOR_GRAY = color(242, 242, 242);
-const COLOR_WHITE = color(255, 255, 255);
-//grid outline
-const COLOR_BLUE = color(135, 206, 235, 100);
-//selectio color
-const COLOR_RED_SELECTION = color(0, 0, 100, TRANSPERENCY);
+// view matrix variables
+var view_matrix = [];
+var VIEW_VALUE = 1;
+var map_height;
+var map_width;
+var changing_view = false;
+var view_index_offset_x = 0;
+var view_index_offset_y = 0;
+const VIEW_STEP_VALUE = 10;
+// changes queue
+var updateQueue = new Array();
+// shifts removes first element and returns it
+// push adds element to end of array
 
-const COLOR_PURPLE = color(0, 91, 156);
-const CANVAS_HEIGHT = 800;
-const CANVAS_WIDTH = 800;
-// const CHUNCK_SIZE = 2;
-const cell_size = 16;
-// var chuncks = new Array();
-var map_grid;
-var draw_grid = false;
-var grid_horizontal_chuncks;
-var grid_vertical_chunks;
+
+
+$(document).ready( () => {
+    console.log('Document is ready');
+    // console.log(test_map_image['height']);
+    Jimp.read('../assets/test-map.png')
+    .then(image => {
+
+        map_height  = image["bitmap"]["height"];
+        map_width  = image["bitmap"]["width"];
+        createGrid();
+        createViewMatrix();
+    })
+    .then( result => {
+        // console.log(result);
+    })
+    .catch(err => {
+        console.log(err);
+        return 0;
+    });
+
+
+
+
+    document.getElementsByClassName("slider")[0].addEventListener("change", event => {
+
+        // value used to change scaling of view
+        VIEW_VALUE = Number(event["currentTarget"]["value"]);
+        changing_view = true;
+        createViewMatrix();
+        // reset offsets
+        view_index_offset_x = 0;
+        view_index_offset_y = 0;
+        draw_grid = true;
+    }, false);
+});
+
+
 function setup() {
     createCanvas( CANVAS_HEIGHT, CANVAS_WIDTH);
     pixelDensity(1);
 }
-
 var test_map_image;
 function preload() {
+
+    // TODO: stalls time, if commented out application crashes, try using jquery to fix
     test_map_image = loadImage('../assets/test-map.png');
 
-    // test_map_image
-    // console.log(test_map_image);
 }
-
-// console.log(test_map_image["height"]["height"]);
-// const grid = createGrid();
 
 
 function createGrid()
 {
-    // initialize grid
-    var length = CANVAS_WIDTH / cell_size;
-    var temp_grid = new Array(length);
 
-    for(var i=0; i < temp_grid.length; i++)
+    // calculate grid dimensions based on map dimensions and processing value
+    var vertical_length = Math.floor( map_height / GRID_PROCESSING_VALUE );
+    var horizontal_length = Math.floor( map_width / GRID_PROCESSING_VALUE);
+
+    grid = new Array( vertical_length );
+
+    for(var i=0; i < grid.length; i++)
     {
-        temp_grid[i] = new Array(length);
+        grid[i] = new Array( horizontal_length );
     }
 
-    // console.log();
-    // console.log(chuncks[0])
-    processImage();
-    // console.log(test_map_image.height);
-    // getHeight();
-    increment_size = ( CANVAS_WIDTH / ( test_map_image.width / cell_size) ) ;
-    // console.log(Math.floor(test_map_image.width / cell_size))
-    // console.log(increment_size);
-    var increment = cell_size;
-    for(var y=0; y < temp_grid.length; y++)
+    for(var y=0; y < grid.length; y++)
     {
-        for(var x=0; x < temp_grid[y].length; x++)
+        for(var x=0; x < grid[y].length; x++)
         {
-            temp_grid[x][y] = new Cell(increment_size*x, increment_size*y, increment_size);
+            grid[x][y] = new Cell(GRID_PROCESSING_VALUE*x, GRID_PROCESSING_VALUE*y, GRID_PROCESSING_VALUE);
         }
     }
-    return temp_grid;
+
+    processImage();
 }
 
-
 function processImage() {
-    // var color_grid;
-    var w;
-    var h;
 
     Jimp.read('../assets/test-map.png')
     .then(image => {
 
-        // image.pixelate( size[x, y, w, h ]);
-        // image.normalize();
-        // console.log('read image successfully');
-        var map_height  = image["bitmap"]["height"];
-        var map_width  = image["bitmap"]["width"];
-        var horizontal_chuncks = Math.floor(map_width / cell_size);
-        w = horizontal_chuncks;
-        h = vertical_chuncks;
-        // chuncks.push(horizontal_chuncks);
-        // chuncks.push(vertical_chuncks);
-        // console.log(horizontal_chuncks);
-        var vertical_chuncks = Math.floor(map_height / cell_size);
-        // console.log('h: %d w: %d',map_height, map_width);
+        // calculate chunking  based on map dimensions and processing value
+        var horizontal_chunks = Math.floor( map_width / GRID_PROCESSING_VALUE );
+        var vertical_chunks = Math.floor( map_height / GRID_PROCESSING_VALUE );
 
-        var color_grid = new Array(vertical_chuncks);
+        var color_grid = new Array( vertical_chunks );
+
 
         for(var i=0; i < grid.length; i++)
         {
-            color_grid[i] = new Array(horizontal_chuncks);
+            color_grid[i] = new Array( horizontal_chunks );
         }
 
 
-
-        for(var y=0; y < vertical_chuncks; y++)
+        // iterate over each chunk
+        for(var y=0; y < vertical_chunks; y++)
         {
-            for(var x=0; x < horizontal_chuncks; x++)
+            for(var x=0; x < horizontal_chunks; x++)
             {
-                var chunk_x_start_position = x * cell_size;
-                var chunk_y_start_position = y * cell_size;
-                var chunk_x_end_position = chunk_x_start_position + cell_size;
-                var chunk_y_end_position = chunk_y_start_position + cell_size;
+                var chunk_x_start_position = x * GRID_PROCESSING_VALUE;
+                var chunk_y_start_position = y * GRID_PROCESSING_VALUE;
+                var chunk_x_end_position = chunk_x_start_position + GRID_PROCESSING_VALUE;
+                var chunk_y_end_position = chunk_y_start_position + GRID_PROCESSING_VALUE;
 
                 for(var _y=chunk_y_start_position; _y < chunk_y_end_position; _y++)
                 {
-                    //process each chunk
+                    //process chunk
                     var pixel_array = new Array();
                     for(var _x=chunk_x_start_position; _x < chunk_x_end_position; _x++)
                     {
-                        // pixel_array.push()
-                        // console.log(Jimp.intToRGBA(image.getPixelColor(_x,_y))["r"]);
+                        // add rgb pixel value to chunk pixel array
                         pixel_array.push( color(Jimp.intToRGBA(image.getPixelColor(_x,_y))["r"], Jimp.intToRGBA(image.getPixelColor(_x,_y))["g"], Jimp.intToRGBA(image.getPixelColor(_x,_y))["b"] ) );
                     }
                 }
@@ -150,10 +175,9 @@ function processImage() {
                 var grass_pixel_count = 0;
                 var letter_pixel_count = 0;
 
-                // console.log(pixel_array[10]["levels"][0]);
                 for(i=0; i < pixel_array.length; i++)
                 {
-
+                    // increment pixel count based on preset colors with liniency
                     if( compareRGBColor( PIXEL_VALUE_LINIENCY, COLOR_ROAD["levels"][0], COLOR_ROAD["levels"][1], COLOR_ROAD["levels"][2], pixel_array[0]["levels"][0], pixel_array[0]["levels"][1], pixel_array[0]["levels"][2]) ) {
                         road_pixel_count++;
                     } else if( compareRGBColor( PIXEL_VALUE_LINIENCY, COLOR_GRASS["levels"][0], COLOR_GRASS["levels"][1], COLOR_GRASS["levels"][2], pixel_array[0]["levels"][0], pixel_array[0]["levels"][1], pixel_array[0]["levels"][2])  ) {
@@ -165,16 +189,8 @@ function processImage() {
                     }
 
                 }
-                // console.log(pixel_array[0]["levels"][0]);
-                // compareRGBColor(COLOR_BLUE["levels"][0], COLOR_BLUE["levels"][1], COLOR_BLUE["levels"][2]);
-                // console.log( compareRGBColor(COLOR_ROAD["levels"][0], COLOR_ROAD["levels"][1], COLOR_ROAD["levels"][2], pixel_array[0]["levels"][0], pixel_array[0]["levels"][1], pixel_array[0]["levels"][2]) );
 
-                // compareRGBColor( pixel_array[0]["levels"], COLOR_BLACK );
-
-
-
-                // console.log('road: %d grass: %d building %d', road_pixel_count, grass_pixel_count, building_pixel_count);
-                // assign prominent structure color
+                // determine prominent pixel color in chunk
                 if(road_pixel_count > building_pixel_count && road_pixel_count > grass_pixel_count && road_pixel_count > letter_pixel_count) {
                     color_grid[x][y] = COLOR_ROAD;
                 } else if(grass_pixel_count > road_pixel_count && grass_pixel_count > building_pixel_count && grass_pixel_count > letter_pixel_count) {
@@ -187,15 +203,11 @@ function processImage() {
                     color_grid[x][y]  = COLOR_ROAD;
                 }
 
-                //process each chunk
-                // var pixel_array = new Array();
+
             }
         }
 
-        // return color_grid;
-        // console.log(color_grid.length);
-        // console.log(color_grid[0].length);
-        // update grid
+        // set cell type based prominent pixel color
         for(var y=0; y < color_grid.length; y++)
         {
             for(var x=0; x < color_grid[y].length; x++)
@@ -211,108 +223,16 @@ function processImage() {
                 }
             }
         }
-        // console.log(grid.length);
 
-        // console.log(Jimp.intToRGBA(image.getPixelColor(32,32))["r"]);
-        // image.greyscale()
-        //     .write('cropped-test.png');
-        // // Do stuff with the image.
-        // return("hello ");
     })
     .catch(err => {
         console.log(err);
         return 0;
-        // Handle an exception.
     });
-    // console.log("w: %d h: %d", w, h);
-    // return color_grid;
 }
-
-function drawGrid(grid)
-{
-    for(var y=0; y < grid.length; y++)
-    {
-        for(var x=0; x < grid[y].length; x++)
-        {
-            // if (!grid[x][y].get_visited) {
-            //     fill(COLOR_BLACK);
-            // }
-            // else {
-            //     fill(COLOR_PURPLE);
-            // }
-
-
-            if (grid[x][y].get_type != null) {
-                if ( grid[x][y].get_type == "road" ) {
-                    fill(COLOR_ROAD);
-                } else if(grid[x][y].get_type == "grass") {
-                    fill(COLOR_GRASS);
-                } else if(grid[x][y].get_type == "building") {
-                    fill(COLOR_BUILDING);
-                } else if(grid[x][y].get_type == "letter") {
-                    fill(COLOR_LETTER);
-            }
-            square(grid[x][y].get_x_position, grid[x][y].get_y_position, grid[x][y].get_size);
-        }
-    }
-}
-}
-
-function drawSelectionOverlayGrid() {
-    for(var y=0; y < grid.length; y++)
-    {
-        for(var x=0; x < grid[y].length; x++)
-        {
-            if (grid[x][y].get_visited) {
-                fill(COLOR_RED_SELECTION);
-                square(grid[x][y].get_x_position, grid[x][y].get_y_position, grid[x][y].get_size);
-            }
-            // else {
-            //     fill(COLOR_PURPLE);
-            // }
-
-
-
-        }
-    }
-
-}
-
-
-function mousePressed() {
-    // processImage();
-    // randomPixels();
-    // console.log(increment_size);
-    var x_selected = Math.floor ( mouseX / increment_size );
-    var y_selected = Math.floor ( mouseY / increment_size );
-
-    if ( ( x_selected >= 0 && x_selected < grid[0].length ) && ( y_selected >= 0 && y_selected < grid.length ) )
-    {
-        grid[x_selected][y_selected].set_visited = (!grid[x_selected][y_selected].get_visited);
-        // console.log('x: %d y: %d', x_selected, y_selected);
-    }
-}
-
-
-
-var grid;
-// const map = processImage();
-
-function keyPressed() {
-    if( keyCode === ENTER ) {
-        grid = createGrid();
-        draw_grid = true;
-        // console.log(test_map_image.height);
-        // console.log( compareRGBColor(COLOR_BLUE, COLOR_BLUE) ) ;
-        // processImage();
-        // console.log(COLOR_BLUE["levels"]);
-    }
-}
-
 
 function compareRGBColor(pixel_liniency, preset_r, preset_g, preset_b, current_r, current_g, current_b) {
-
-//    console.log(pixel_liniency);
+    // compares rbg values with pixel liniency
     if( ( current_r >=  ( preset_r - pixel_liniency) && current_r <=  ( preset_r + pixel_liniency) ) && ( current_g >=  ( preset_g - pixel_liniency) && current_g <=  ( preset_g + pixel_liniency) ) && ( current_b >=  ( preset_b - pixel_liniency) && current_b <=  ( preset_b + pixel_liniency) ) )  {
         return true;
     } else {
@@ -320,17 +240,157 @@ function compareRGBColor(pixel_liniency, preset_r, preset_g, preset_b, current_r
     }
 }
 
-function draw() {
-    // processImage();
-    // background(COLOR_BLACK);
-    stroke(COLOR_WHITE);
-    // background(51);
-    if (draw_grid) {
+function createViewMatrix() {
 
-        drawGrid(grid);
-        drawSelectionOverlayGrid();
-
+    // calculate grid dimensions
+    vertical_chunks = Math.floor( grid.length / VIEW_VALUE );
+    horizontal_chunks = Math.floor( grid[0].length / VIEW_VALUE );
+    // initialize grid
+    view_matrix = new Array( vertical_chunks );
+    for(var i=0; i < view_matrix.length; i++)
+    {
+        view_matrix[i] = new Array( horizontal_chunks );
     }
-    // selectionOverlayGrid();
-    // randomPixels();
+
+    // insert grid coordinate values
+    for(var y=0; y < view_matrix.length; y++)
+    {
+        for(var x=0; x < view_matrix[y].length; x++)
+        {
+            view_matrix[x][y] = [ x, y ];
+        }
+    }
+
+
+}
+
+function drawGrid( current_grid )
+{
+
+    if (current_grid != null)
+    {
+        var view_matrix_increment_value = Math.floor( CANVAS_WIDTH / current_grid.length )
+        for(var y=0; y < current_grid.length; y++)
+        {
+            for(var x=0; x < current_grid[y].length; x++)
+            {
+
+                if(current_grid[x][y] != null) {
+
+                    // check cell type
+                    if ( grid[ current_grid[x][y][0] + view_index_offset_x ][ current_grid[x][y][1] + view_index_offset_y ] != null )
+                    {
+
+                        if ( grid[ current_grid[x][y][0] + view_index_offset_x ][ current_grid[x][y][1] + view_index_offset_y ].get_type == 'road') {
+                            fill( COLOR_ROAD );
+                        } else if ( grid[ current_grid[x][y][0] + view_index_offset_x ][ current_grid[x][y][1] + view_index_offset_y ].get_type == 'grass') {
+                            fill( COLOR_GRASS );
+                        } else if ( grid[ current_grid[x][y][0] + view_index_offset_x ][ current_grid[x][y][1] + view_index_offset_y ].get_type == 'building') {
+                            fill( COLOR_BUILDING );
+                        } else if ( grid[ current_grid[x][y][0] + view_index_offset_x ][ current_grid[x][y][1] + view_index_offset_y ].get_type == 'letter') {
+                            fill( COLOR_LETTER );
+                        }
+
+                    }
+
+                    //draw cell
+                    square(view_matrix_increment_value*x, view_matrix_increment_value*y, view_matrix_increment_value);
+                }
+
+            }
+        }
+
+
+    } else {
+        console.log('no view matrix');
+    }
+
+
+}
+
+
+function mousePressed() {
+    // calculate coordinates of chunk selected, accounts for view offset
+    var view_matrix_increment_value = Math.floor( CANVAS_WIDTH / view_matrix.length )
+    var x_selected = Math.floor ( mouseX / view_matrix_increment_value ) + view_index_offset_x;
+    var y_selected = Math.floor ( mouseY / view_matrix_increment_value ) + view_index_offset_y;
+    console.log('x: %d y: %d', x_selected, y_selected);
+
+}
+
+function keyPressed() {
+    if( keyCode === ENTER ) {
+        console.log( grid );
+        console.log( view_matrix );
+    }
+
+    // move -Y direction
+    if ( keyCode ===  87 ) {
+        // console.log('pressed w');
+        for( var i=0; i< VIEW_STEP_VALUE; i++) {
+            // checking vertical grind bounds
+            if ( !( view_index_offset_y - 1 < 0 ) ) {
+                view_index_offset_y -= 1;
+                // drawGrid( view_matrix );
+                draw_grid = true;
+            }
+        }
+        // console.log( view_index_offset_y );
+    }
+
+    // move on +Y direction
+    if ( keyCode === 83 ) {
+        // console.log('pressed s');
+        for( var i=0; i< VIEW_STEP_VALUE; i++) {
+            //checking vertical grid bounds
+            if ( ( ( view_matrix.length - 1 ) + view_index_offset_y + 1 < grid.length ) && ( grid[ 0 ][ ( view_matrix.length - 1 ) + view_index_offset_y + 1] != null )){
+                view_index_offset_y += 1;
+                // drawGrid( view_matrix );
+                draw_grid = true;
+            }
+        }
+        // console.log( view_index_offset_y );
+    }
+
+    // move on -X direction
+    if ( keyCode === 65 ) {
+        // console.log('pressed a');
+        for( var i=0; i< VIEW_STEP_VALUE; i++) {
+            // checking horizontal grid bounds
+            if ( !( view_index_offset_x - 1 < 0 ) ) {
+                view_index_offset_x -= 1;
+                // drawGrid( view_matrix );
+                draw_grid = true;
+            }
+        }
+        // console.log( view_index_offset_x );
+    }
+
+    // move on +X direction
+    if ( keyCode === 68 ) {
+        // console.log('pressed d');
+        for( var i=0; i< VIEW_STEP_VALUE; i++) {
+             // checking horizontal grid bounds
+            if ( ( ( view_matrix[0].length - 1 ) + view_index_offset_x + 1 < grid[0].length ) && ( grid[ ( view_matrix[0].length - 1 ) + view_index_offset_x + 1 ][ 0 ] != null )){
+                view_index_offset_x += 1;
+                // drawGrid( view_matrix );
+                draw_grid = true;
+            }
+        }
+        // console.log( view_index_offset_x );
+    }
+
+
+}
+
+function draw() {
+    stroke( COLOR_BLUE );
+
+    if ( draw_grid ) {
+        background( COLOR_WHITE );
+        drawGrid( view_matrix );
+        draw_grid = false;
+        console.log('drawing map');
+    }
+
 }
